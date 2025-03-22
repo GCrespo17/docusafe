d12d276fac524a35729d1f7349f413e4ab527743
Gabriel <gcrespo1705@gmail.com>
2025-03-22 15:43:30 -0400

    Ejecuta los servicios

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,6 +1,8 @@
 const connectButton = document.getElementById('connect-button');
 const address = document.getElementById('address');
 
+
+
 if(typeof window.ethereum !== 'undefined'){
     console.log('MetaMask is installed!');
 }
@@ -8,13 +10,36 @@ if(typeof window.ethereum !== 'undefined'){
 connectButton.addEventListener('click', async ()=>{
 
     const avalancheInitialized = await AvalancheService.initialize();
+    const ipfsInitialized = await IPFSService.initialize();
 
-    // console.log('Connecting');
-    // const accounts=await window.ethereum.request({method: 'eth_requestAccounts'});
-
-    // const account=accounts[0];
     const account= await AvalancheService.getCurrentAccount();
     console.log(account);
     address.textContent=`${account}`;
-
+    if (typeof AvalancheService !== 'undefined' && AvalancheService.web3) {
+        console.log('vacio');
+        console.log(AvalancheService);
+    }
 });
+
+async function uploadDocument(event){
+    //Este prevent default es para que la pagina no recarge o se redireccione a un url, lo que usualmente hace
+    event.preventDefault();
+
+    const status=document.getElementById('document-status');
+    const file=document.getElementById('document-file');
+    const name=document.getElementById('document-name');
+    try{
+        status.textContent='Subiendo a IPFS';
+        const ipfsHash= await IPFSService.uploadDocument(file.files[0]);
+
+        status.textContent='Documento subido exitosamente';
+
+        event.target.reset();
+
+    }catch(error){
+        console.error('Error al subir el archivo', error);
+        status.textContent='Error al subir el archivo';
+    }
+}
+
+document.getElementById('upload-form').addEventListener('submit', uploadDocument);
