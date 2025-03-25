6e490d4f43c65f83f93c6996d28e85f5c0a48496
Gabriel <50721663+GCrespo17@users.noreply.github.com>
2025-03-25 19:28:10 -0400

    Se cambio el codigo ipfs para que pueda ser hosteado en pinata en vez de local

--- frontend/scripts/ipfs-service.js
+++ frontend/scripts/ipfs-service.js
@@ -1,31 +1,47 @@
-const IPFSService={
-    ipfs: null,
-    gateway: 'http://localhost:8080/ipfs/',
+const IPFSService = {
+  pinataJwt: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5NzgzOGU3NS1iYzlhLTQyMmUtOWU0Mi0wNmIyYWRmN2JiMjQiLCJlbWFpbCI6ImdjcmVzcG8xNzA1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJjN2E0NjRhMjJmMTNmZjJhNmJmMSIsInNjb3BlZEtleVNlY3JldCI6IjliYmM5N2RhMDBjZTYzZDFhYWFiMTE5YjVjMGMzZTljZTMwZmNlZmE1NDY5ODczYTZjMWVlZTI5MWNiY2RhZGUiLCJleHAiOjE3NzQ0NzYxMzl9.YqENg-0D8ketAqOROa8gKnp3MbcHxeGZ1T_xBEN4syw",
+  gateway: "https://gateway.pinata.cloud/ipfs/",
 
-    async initialize(){
-        this.ipfs = await window.IpfsHttpClient.create({
-            host: 'localhost',
-            port: '5001',
-            protocol: 'http'
-        });
-        console.log(this.ipfs);
-        return true;
-    },
+  async uploadDocument(file) {
+      try {
+          const formData = new FormData();
+          formData.append('file', file);
 
-    async uploadDocument(file) {
-        try {
-          const result = await this.ipfs.add(file);
-          const cid = result.cid.toString();
-          console.log('Documento subido a IPFS. CID:', cid);
-          return cid;
-        } catch (error) {
-            console.error('Error al subir el documento a IPFS:', error);
-            throw error;
-        }
-      },
-      
-      
-      getDocumentUrl(ipfsHash) {
-        return `${this.gateway}${ipfsHash}`;
+          //Agregar metadatos
+          const metadata = {
+              name: file.name,
+              keyvalues: {
+                  app: "Docusafe",
+              }
+          };
+          formData.append('pinataMetadata', JSON.stringify(metadata));
+
+
+          // Enviar a Pinata
+          const response = await fetch('https://api.pinata.cloud/pinning/pinFileToIPFS', {
+              method: 'POST',
+              headers: {
+                  'Authorization': this.pinataJwt
+              },
+              body: formData
+          });
+
+          if (!response.ok) {
+              const errorData = await response.json();
+              console.error("Error details:", errorData);
+              throw new Error(errorData.error || "Error al subir");
+          }
+
+          const result = await response.json();
+          return result.IpfsHash; // Retorna el CID
+
+      } catch (error) {
+          console.error("Error en IPFSService:", error);
+          throw new Error(`Falló la subida: ${error.message}`);
       }
+  },
+
+  getDocumentUrl(ipfsHash) {
+      return `${this.gateway}${ipfsHash}`;
+  }
 };
