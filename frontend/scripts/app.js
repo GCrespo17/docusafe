dd46bae4bd96a7f95371b99253a62a89c116767b
Diana V Rodríguez L <dvrodriguez.22@est.ucab.edu.ve>
2025-03-24 23:38:04 -0400

    Update app.js
    Interaccion interfaz con los servicios

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -16,7 +16,6 @@ connectButton.addEventListener('click', async ()=>{
     console.log(account);
     address.textContent=`${account}`;
     if (typeof AvalancheService !== 'undefined' && AvalancheService.web3) {
-        console.log('vacio');
         console.log(AvalancheService);
     }
 });
@@ -32,6 +31,9 @@ async function uploadDocument(event){
         status.textContent='Subiendo a IPFS';
         const ipfsHash= await IPFSService.uploadDocument(file.files[0]);
 
+        status.textContent='Registrando el documento en avalanche';
+        await AvalancheService.addDocument(ipfsHash, name.value);
+
         status.textContent='Documento subido exitosamente';
 
         event.target.reset();
@@ -42,4 +44,39 @@ async function uploadDocument(event){
     }
 }
 
+function showAllDocuments(listId, documents){
+    const list = document.getElementById(listId);
+    list.innerHTML = '';
+
+    if (documents.length === 0) {
+        listElement.innerHTML = '<li class="no-docs">No documents found</li>';
+        return;
+    }
+
+    documents.forEach(doc =>{
+        const li=document.createElement('li');
+        
+        const documentUrl = IPFSService.getDocumentUrl(doc.ipfsHash);
+
+        const name=document.createElement('h3');
+        name.textContent=doc.name;
+        const url = document.createElement('a');
+        url.href = documentUrl;
+        url.textContent = 'View Document';
+        url.target = '_blank'; // Abrir en una nueva pestaña
+
+        li.appendChild(name);
+        li.appendChild(url);
+
+        list.appendChild(li);
+    });
+
+}
+
+async function loadDocuments(){
+    const documents = await AvalancheService.getAllDocuments();
+    showAllDocuments('document-list', documents)
+}
+
 document.getElementById('upload-form').addEventListener('submit', uploadDocument);
+document.getElementById('refresh-btn').addEventListener('click', loadDocuments);
